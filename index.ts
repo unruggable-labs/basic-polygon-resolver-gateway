340b24702932c1d412ab3b93f25518cc9478603c
Darian Bailey <darian@nftychat.xyz>
2024-10-03 22:41:30 -0400

    more logging

--- index.ts
+++ index.ts
@@ -3,9 +3,18 @@ import serverAdapter from "./server-adapter";
 
 const app = express();
 
-// Bind our adapter to `/mypath` endpoint
+console.log("Starting server...");
+
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request to ${req.url}`);
+  next();
+});
+
+// Bind our adapter to `/` endpoint
 app.use("/", serverAdapter);
 
-app.listen(4000, () => {
-  console.log("Running the server at http://localhost:4000/");
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
 });
